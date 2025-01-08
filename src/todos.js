5c21c5ace29d0deb6af6809db75a1e9e052b1213
ggang89 <ggang89@gmail.com>
2025-01-08 21:41:07 +0900

    바닐라 js로 만든 기본 form TODO

---
+++ src/todos.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const createTodo = (todo) => {
+  const li = document.createElement("li");
+  li.innerText = todo;
+  ul.appendChild(li);
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const todo = input.value;
+  input.value = "";
+  createTodo(todo);
+};
+
+form.addEventListener("submit", onSubmit);
