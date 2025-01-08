18496d3b70e3d60b3159a398e003fc2b6fba5407
ggang89 <ggang89@gmail.com>
2025-01-08 19:13:38 +0900

    refactoring

--- src/main.js
+++ src/main.js
@@ -6,13 +6,30 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+// 오류방지를 위해 string을 넣은 변수 사용
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const reducer = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // if (action.type === "ADD") {
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+
+  // switch 문으로 변경
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+      break;
+    case MINUS:
+      return count - 1;
+      break;
+    default:
+      return count;
+      break;
   }
 };
 
@@ -21,10 +38,11 @@ const countStore = createStore(reducer);
 const onChange = () => {
   number.innerText = countStore.getState();
 };
-countStore.subscribe(onChange);
+
+countStore.subscribe(onChange); // store에서 변화를 감지
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: "ADD" }); // 액션은 무조건 객체형태
 };
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
