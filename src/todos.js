bae698877af38391e02fee1c5205ffe18060ffbf
ggang89 <ggang89@gmail.com>
2025-01-08 22:51:09 +0900

    redux로 새로운 투두 추가하기

--- src/todos.js
+++ src/todos.js
@@ -1,18 +1,49 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createTodo = (todo) => {
-  const li = document.createElement("li");
-  li.innerText = todo;
-  ul.appendChild(li);
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { text: action.text, id:Date.now() }];
+    case "DELETE_TODO":
+      return [];
+
+    default:
+      return state;
+  }
 };
+
+const todoStore = createStore(reducer);
+
+todoStore.subscribe(() => console.log(todoStore.getState()));
+
+const paintTodo = () => {
+  const todos = todoStore.getState();
+  todos.forEach(todo => {
+    
+    const li = document.createElement("li");
+    li.id = todo.id;
+    li.innerText = todo.text;
+    ul.appendChild(li);
+  })
+
+};
+
+todoStore.subscribe(paintTodo);
+
+
+const addTodo = (text) => {
+  todoStore.dispatch({ type: "ADD_TODO", text });
+}
 
 const onSubmit = (e) => {
   e.preventDefault();
   const todo = input.value;
   input.value = "";
-  createTodo(todo);
+  addTodo(todo);  
 };
 
 form.addEventListener("submit", onSubmit);
